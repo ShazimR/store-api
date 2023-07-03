133dd18b47d975357fc59461811fb9fca0e6ba14
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-03 15:56:29 -0400

    added pages and page limits to queries

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if (featured) queryObject.featured = featured === 'true';
@@ -17,10 +17,19 @@ const getAllProducts = async (req, res) => {
     
     if (sort) {
         const sortList = sort.split(',').join(' ');
-        result.sort(sortList);
-    } else {
-        result = result.sort('createdAt');
+        result = result.sort(sortList);
+    } else result = result.sort('name -price');
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
 
     const products = await result;
     res.status(200).json({products, nbHits: products.length});
