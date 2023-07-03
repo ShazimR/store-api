efa3fb27b60e132d1f53c086ef7f59f9a6f6e67e
Shazim Rahman <shazimrahman@hotmail.com>
2023-07-03 15:39:04 -0400

    added sorting by fields for query results

--- controllers/products.js
+++ controllers/products.js
@@ -6,15 +6,23 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {};
 
     if (featured) queryObject.featured = featured === 'true';
     if (company) queryObject.company = company;
     if (name) queryObject.name = { $regex: name, $options: 'i' };
+    
+    let result = Product.find(queryObject);
+    
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
 
-    console.log(queryObject);
-    const products = await Product.find(queryObject);
+    const products = await result;
     res.status(200).json({products, nbHits: products.length});
 };
 
